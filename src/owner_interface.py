44284da0a7dd1eb3d307e88c2a55e2ebf2ae20fa
Param3103 <param.mehrotra@gmail.com>
2021-03-08 21:25:07 +0800

    commiting final changes of first stage of project

--- src/owner_interface.py
+++ src/owner_interface.py
@@ -2,7 +2,8 @@ from stock_management import stock_management
 from seller_management import seller_management
 
 task = int(input("Hello Param, what will you like to do today?\nEnter 1 if you would like to add new type of items to your store.\
-             \nEnter 2 if you would like to replenish existing stock.\nEnter 3 if you would like to add new vendors to your list."))
+             \nEnter 2 if you would like to replenish existing stock.\nEnter 3 if you would like to add new vendors to your list.\
+                 \nEnter 4 if you would like to undate details of your vendor."))
 if task == 1:
     print("Adding new type of item to store.")
     stock_type = input("What type of item are you adding to the store? ")
@@ -19,3 +20,24 @@ elif task == 3:
     email = input("What is his email id? ")
     address = input("What is his address? ")
     seller_management.add_new_seller(name, phone, email, address)
+elif task == 4:
+    print("Updating details of vendors.")
+    num_identifying_keys = int(input('How many details do you have to identify the vendor whose details you need to update? '))
+    identifying_keys = []
+    identifying_values = []
+    for num in range(num_identifying_keys):
+        identifying_key = input(
+            'what kind of detail do you have? \nName, Phone, EmailID or Address. Plz input in same form.')
+        identifying_keys.append(identifying_key)
+        identifying_value = input('Plz input the contact detail now. ')
+        identifying_values.append(identifying_value)
+    num_tbc_keys = int(input('How many details of this contact do you want to change? '))
+    tbc_keys = []
+    new_values = []
+    for num in range(num_tbc_keys):
+        tbc_key = input(
+            'what kind of detail do you want to change? \nName, Phone, EmailID or Address. Plz input in same form.')
+        tbc_keys.append(tbc_key)
+        new_value = input('Plz input the new contact detail now. ')
+        new_values.append(new_value)
+    seller_management.update_seller_contact(identifying_keys, identifying_values, tbc_keys, new_values)
--- src/seller_management.py
+++ src/seller_management.py
@@ -1,14 +1,47 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    database="param_super_mart"
-)
 
-mycursor = mydb.cursor()
 
 class seller_management:
     def add_new_seller(name, phone, email, address):
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            database="param_super_mart"
+        )
+
+        mycursor = mydb.cursor()
         command = "insert into seller_details(name, phone, email, address) values (\'{0}\', \'{1}\', \'{2}\', \'{3}\');".format(name, phone, email, address)
         mycursor.execute(command)
+        mydb.commit()
+
+    def update_seller_contact(finder_keys, finder_values, tbc_keys, new_values):
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            database="param_super_mart"
+        )
+
+        mycursor = mydb.cursor()
+        command = "UPDATE seller_details set "
+        for tbc_key in tbc_keys:
+            command += tbc_key
+            command += '='
+            command += '\''
+            command += new_values[tbc_keys.index(tbc_key)]
+            command += '\''
+            if tbc_keys[-1] != tbc_key:
+                command += ', '
+        command += ' WHERE '
+        for finder_key in finder_keys:
+            command += finder_key
+            command += '='
+            command += '\''
+            command += finder_values[finder_keys.index(finder_key)]
+            command += '\''
+            if finder_keys[-1] != finder_key:
+                command += ', '
+            else:
+                command += ';'
+        mycursor.execute(command)
+        mydb.commit()
