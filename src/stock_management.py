60f34af9331232a4aafee6325dffc7d7961f8213
Param3103 <param.mehrotra@gmail.com>
2021-05-11 12:24:02 +0800

    Adding updates to test

--- src/stock_management.py
+++ src/stock_management.py
@@ -13,21 +13,27 @@ class stock_management:
         command = "insert into stock_type(stock_type) values (\"{}\");".format(item_type)
         mycursor.execute(command)
         mydb.commit()
-        print("{} has been added into the database.".format(item_type))
         # done
 
-    def adding_new_stock(stock_name, type, quantity, price_when_bought, seller, price_for_customers):
+    def adding_new_stock(stock_name, type, quantity, price_when_bought, seller_id, price_for_customers):
+
         mydb = mysql.connector.connect(
             host="localhost",
             user="root",
             database="param_super_mart"
         )
+        my_cursor = mydb.cursor()
 
-        mycursor = mydb.cursor()
-        type_id = mycursor.execute("select type_id from stock_type where stock_type=\'{0}\';".format(type))
-        seller_id = mycursor.execute("select seller_id from seller_details where name=\'{0}\';".format(seller))
+        price_for_customers = float(price_for_customers)
 
-        mycursor.execute(("insert into stock_details(stock_name, type_id, quantity, sale_price, seller_id, purchase_price) values(\'{3}\', {4}, {0}, {1}, {5}, {2});".format(quantity, price_for_customers, price_when_bought, stock_name, type_id, seller_id)))
+        type_id_command = "select type_id from stock_type where stock_type=\'" + type + "\';"
+        my_cursor.execute(type_id_command)
+
+        type_id = my_cursor.fetchall()
+
+        my_cursor.execute("INSERT INTO stock_details (stock_name, type_id, quantity, sale_price, seller_id,\
+         purchase_price) VALUES (\'{3}\', {4}, {0}, {1}, {5}, {2});".format(quantity, price_for_customers, \
+        price_when_bought, stock_name, type_id[0][0], seller_id))
         mydb.commit()
         print("new stock has been added.")
 
@@ -39,8 +45,11 @@ class stock_management:
         )
 
         mycursor = mydb.cursor()
-        current_qty = int(mycursor.execute("select quantity from stock_details where stock_name=\'{0}\';".format(name)))
+        mycursor.execute("select quantity from stock_details where stock_name=\'{0}\';".format(name))
+        current_qty = mycursor.fetchall()
+        current_qty = current_qty[0][0]
         new_qty = current_qty + quantity
-        mycursor.execute("update stock_details set quantity={1} where stock_name=\'{0}\'".format(name,new_qty))
+        mycursor.execute("update stock_details set quantity={1} where stock_name=\'{0}\';".format(name,new_qty))
         mydb.commit()
+
 stock_management
--- tests/test_stock_management.py
+++ tests/test_stock_management.py
@@ -10,17 +10,25 @@ class Testing_Stock_Management(unittest.TestCase):
             database="param_super_mart"
         )
         self.my_cursor = self.mydb.cursor()
+
     def tearDown(self):
-        self.my_cursor.execute("delete from stock_type;")
-        self.my_cursor.execute(
-            "ALTER TABLE stock_type AUTO_INCREMENT = 1;")
+        self.mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            database="param_super_mart"
+        )
+        self.my_cursor = self.mydb.cursor()
         self.my_cursor.execute("delete from stock_details;")
         self.my_cursor.execute(
             "ALTER TABLE stock_details AUTO_INCREMENT = 1;")
         self.my_cursor.execute("delete from seller_details;")
         self.my_cursor.execute(
             "ALTER TABLE seller_details AUTO_INCREMENT = 1;")
+        self.my_cursor.execute("delete from stock_type;")
+        self.my_cursor.execute(
+            "ALTER TABLE stock_type AUTO_INCREMENT = 1;")
         self.mydb.commit()
+
 
     def test_adding_new_type(self):
         stock_management.adding_new_type("Coconut")
@@ -29,19 +37,25 @@ class Testing_Stock_Management(unittest.TestCase):
         self.assertIn((1, 'Coconut'), values)
         # done
     def test_adding_new_stock(self):
-# STILL NEED TO WORK ON THIS
        # create new stock type
        stock_management.adding_new_type("Coconut")
        self.mydb.commit()
+
        # create new seller
-       seller_management.add_new_seller('Raj', None, None, None)
+       seller_management.add_new_seller("Raj", "","", "")
        self.mydb.commit()
+
        # add new stock...
-       stock_management.adding_new_stock("Raj's Coconut", "Coconut", 50, 5.00, "Raj", 5.50)
+       stock_management.adding_new_stock("Raj Coconut", "Coconut", 50, 5.00, 1, 5.50)
        self.mydb.commit()
+
+       # test
        self.my_cursor.execute("SELECT * FROM stock_details;")
        values = self.my_cursor.fetchall()
-       self.assertIn([1, "Raj's Coconut", 1, 50, 5.00, 1, 5.50], values)
+       print(values)
+
+       self.assertIn((1, 'Raj Coconut', 1, 50, 5.5, 1, 5.0), values)
+        # done
     def test_replenishing_stock(self):
         # create new stock type
         stock_management.adding_new_type("Coconut")
@@ -50,10 +64,15 @@ class Testing_Stock_Management(unittest.TestCase):
         seller_management.add_new_seller('Raj', None, None, None)
         self.mydb.commit()
         # add new stock...
-        stock_management.adding_new_stock("Raj's Coconut", "Coconut", 50, 5.00, "Raj", 5.50)
+        stock_management.adding_new_stock("Raj Coconut", "Coconut", 50, 5.00, 1, 5.50)
         self.mydb.commit()
-        stock_management.replenishing_stock("Raj's Coconut", 50)
-        qty = self.my_cursor.execute("select quantity from stock_details;")
-        self.assertEqual(qty, 100)
+
+        stock_management.replenishing_stock("Raj Coconut", 50)
+        self.mydb.commit()
+
+        self.my_cursor.execute("select quantity from stock_details;")
+        qty = self.my_cursor.fetchall()
+        self.assertEqual(qty[0][0], 100)
+
 if __name__ == '__main__':
     unittest.main()
