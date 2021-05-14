aeedb2121ffde71ffabacf8bb564ca39a6a39c00
Param3103 <param.mehrotra@gmail.com>
2021-05-14 17:09:10 +0800

    test seller management done

--- tests/test_seller_management.py
+++ tests/test_seller_management.py
@@ -1,6 +1,6 @@
 import unittest
 import mysql.connector
-from src.seller_management import seller_management
+from src.seller_management import SellerManagement
 class Testing_Seller_Management(unittest.TestCase):
     def setUp(self):
         self.my_db = mysql.connector.connect(
@@ -16,15 +16,15 @@ class Testing_Seller_Management(unittest.TestCase):
         self.my_db.commit()
 
     def test_add_new_seller(self):
-        seller_management.add_new_seller("Param", "+65 9427 6963", "", "")
+        SellerManagement.add_new_seller("Param", "+65 9427 6963", "", "")
         self.my_db.commit()
         self.my_cursor.execute("SELECT * FROM seller_details;")
         values = self.my_cursor.fetchall()
         self.assertIn((1, 'Param', '+65 9427 6963', '', ''), values)
     def test_update_seller_contact(self):
-        seller_management.add_new_seller("Param", "+65 9427 6963", "", "")
+        SellerManagement.add_new_seller("Param", "+65 9427 6963", "", "")
         self.my_db.commit()
-        seller_management.update_seller_contact(['Name'], ['Param'], ['Email'], ['param@gmail.com'])
+        SellerManagement.update_seller_contact(['Name'], ['Param'], ['Email'], ['param@gmail.com'])
         self.my_db.commit()
         self.my_cursor.execute("SELECT * FROM seller_details;")
         values = self.my_cursor.fetchall()
